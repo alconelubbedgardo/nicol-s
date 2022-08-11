5e6d5c547591fe1d6fbd1ef137143d2e00dfd4a5
David Emanuel Sandoval <davidemanuelsandoval@gmail.com>
2022-08-11 18:50:05 -0300

    se crearon algunas funciones relacionadas con el login

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+
 
 # Create your views here.
+
+def login_view(request):
+    return render(request, 'accounts/login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('index')
