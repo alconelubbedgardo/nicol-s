e2630533c9721437cb9348fce3a58de50e644152
David Emanuel Sandoval <davidemanuelsandoval@gmail.com>
2022-08-18 19:43:57 -0300

    code refactor

--- blog/views.py
+++ blog/views.py
@@ -89,14 +89,16 @@ def comment_delete(request, pk):
 
 def comment_update(request, pk):
     # buscamos el post y lo mostramos
-    context = {}
+    # context = {}
     comment = get_object_or_404(Comments, id=pk)
     form = CommentForm(request.POST or None, instance=comment)
-    context['comment'] = comment
-    context['post'] = comment.post
+    context = {
+        'comment': comment,
+        'post': comment.post,
+        'form': form,
+    }
     if request.method == 'POST':
         if form.is_valid():
             form.save()
             return redirect('post_detail', pk=comment.post.pk)
-    context['form'] = form
     return render(request, 'blog/comment_edit.html', context)
