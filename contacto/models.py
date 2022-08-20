03c1d54829c88994acef73404cb65694b76a5acb
Martin <martin_e.a@hotmail.com>
2022-08-20 20:14:09 -0300

    agregado app consultas

---
+++ contacto/models.py
@@ -0,0 +1,22 @@
+from random import choices
+from django.db import models
+
+# Create your models here.
+opciones_consultas = [
+    [0, "Consulta"],
+    [1, "Reclamo"],
+    [2, "Sugerencia"],
+    [3, "Felicitaciones"] 
+]
+
+
+class Contacto(models.Model):
+    nombre = models.CharField(max_length=50)
+    correo = models.EmailField()
+    tipo_consulta = models.models.IntegerField(choices = opciones_consultas)
+    mensaje = models.TextField()
+    avisos = models.BooleanField()
+    
+    def __str__(self) -> str:
+        return self.nombre
+    
