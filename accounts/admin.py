ff965c82367f80d144807729161a9336dee965ff
Martin <martin_e.a@hotmail.com>
2022-08-27 19:56:17 -0300

    agregado vista de blog en pag principal

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
+# class CustomUserAdmin(UserAdmin):
+#     list_display = ('username', 'first_name', 'last_name', 'dni', 'email','ROLE_CHOICES',)
+#     fieldsets:
+
+
 
 admin.site.register(User, UserAdmin)
--- home/views.py
+++ home/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from blog.models import Post
+from blog.views import post_detail
 
 # Create your views here.
 
 
+
 def index(request):
-    return render(request, 'home/index.html')
+    post = Post.objects.last()
+    post1 = {'Post': post 
+    }
+    return render(request, 'home/index.html', post1)
+
