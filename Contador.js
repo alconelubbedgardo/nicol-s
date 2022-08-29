9b70f8bc0e34a174b711e2b4611bafc28a34ce73
Edgardo Nicolas <nelubbalcorta@gmail.com>
2022-08-29 10:32:13 -0300

    Contador de visitas en js

---
+++ Contador.js
@@ -0,0 +1,5 @@
+
+<div id="sfcwqhyfu2g2kd6fg5mmmzkjtdgyxdgsnq7"></div>
+<script type="text/javascript" src="https://counter9.stat.ovh/private/counter.js?c=wqhyfu2g2kd6fg5mmmzkjtdgyxdgsnq7&down=async&FCS_rest=tabRT" async></script>
+<noscript><a href="https://www.contadorvisitasgratis.com" title="contadores de visitas"><img src="https://counter9.stat.ovh/private/contadorvisitasgratis.php?c=wqhyfu2g2kd6fg5mmmzkjtdgyxdgsnq7" border="0" title="contadores de visitas" alt="contadores de visitas"></a></noscript>
+
